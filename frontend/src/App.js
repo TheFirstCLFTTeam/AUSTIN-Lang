56cb3326cd185cf3cafbd321c2a58ab4a1235c6f
Gavin <www.inganeh@gmail.com>
2026-02-01 14:38:46 +0800

    feat: added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,34 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+
 import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
 import SubmittedFilesPage from "./pages/SubmittedFilesPage";
 import FileDetailPage from "./pages/FileDetailPage";
+import LoginPage from "./pages/LoginPage";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/files" element={<SubmittedFilesPage />} />
-        <Route path="/files/:id" element={<FileDetailPage />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      {/* Public route */}
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* Protected routes */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="files" element={<SubmittedFilesPage />} />
+        <Route path="files/:id" element={<FileDetailPage />} />
+      </Route>
+    </Routes>
   );
 }
