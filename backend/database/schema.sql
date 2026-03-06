d910613c4f72227ff789bbd9fe26193a47ef9631
WangHowe <wanghowe12345@gmail.com>
2026-03-06 16:17:47 +0800

    Fix merge issues

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:backend/database/schema.sql
 CREATE TABLE IF NOT EXISTS audio_file (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     file_name TEXT NOT NULL,
@@ -36,43 +35,4 @@ CREATE TABLE IF NOT EXISTS edited_transcript_segment (
     end REAL,
     text TEXT,
     FOREIGN KEY (edited_transcript_id) REFERENCES edited_transcript(id)
-=======
-CREATE TABLE IF NOT EXISTS audio_file (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    file_name TEXT NOT NULL,
-    uploaded_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS raw_transcript (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    rating INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    audio_file_id INTEGER,
-    FOREIGN KEY (audio_file_id) REFERENCES audio_file(id)
-);
-
-CREATE TABLE IF NOT EXISTS raw_transcript_segment (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    raw_transcript_id INTEGER,
-    start REAL,
-    end REAL,
-    text TEXT,
-    FOREIGN KEY (raw_transcript_id) REFERENCES raw_transcript(id)
-);
-
-CREATE TABLE IF NOT EXISTS edited_transcript (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    raw_transcript_id INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (raw_transcript_id) REFERENCES raw_transcript(id)
-);
-
-CREATE TABLE IF NOT EXISTS edited_transcript_segment (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    edited_transcript_id INTEGER,
-    start REAL,
-    end REAL,
-    text TEXT,
-    FOREIGN KEY (edited_transcript_id) REFERENCES edited_transcript(id)
->>>>>>> origin/main:database/schema.sql
 );
