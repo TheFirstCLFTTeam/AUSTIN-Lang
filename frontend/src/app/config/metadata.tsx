69464b2b8cb803c4fbf200e029c62b501e9cf001
Larry <larry.lee.jia.heng@gmail.com>
2026-03-18 16:44:24 +0800

    feat: ui shift for basic migration complete

---
+++ frontend/src/app/config/metadata.tsx
@@ -0,0 +1,8 @@
+export const siteConfig = {
+    title: "AUSTIN-Lang: Automated Sound Transcription for Improvement on Non-english Languages.",
+    description: "AUSTIN- Lang is an open-source platform focused on advancing automated sound transcription for underrepresented and non - English languages.Our mission is to empower communities and researchers with accessible tools to improve language documentation, preservation, and accessibility through state - of - the - art speech technology.",
+    ogImage: "https://github.com/TheFirstCLFTTeam/AUSTIN-Lang/blob/main/assets/images/team-logo/png/clft-team-logo-light.png",
+    openGraph: {
+        images: ['/frontend/public/logo.png'],
+    },
+};
